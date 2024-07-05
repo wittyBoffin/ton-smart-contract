c306304c544ee0191a7a0ec9b22ade63a8a7ec92
wittyBoffin <maxcore1120@gmail.com>
2024-07-05 10:57:46 +0200

    added deployCounter script

--- scripts/deployCounter.ts
+++ scripts/deployCounter.ts
@@ -1,13 +1,56 @@
-import { toNano } from '@ton/core';
-import { Counter } from '../wrappers/Counter';
-import { compile, NetworkProvider } from '@ton/blueprint';
+import * as fs from "fs";
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { mnemonicToWalletKey } from "ton-crypto";
+import { TonClient, Cell, WalletContractV4 } from "@ton/ton";
+import Counter from "../wrappers/Counter";
 
-export async function run(provider: NetworkProvider) {
-    const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
+export async function run() {
+    // initialize ton rpc client on testnet
+    const endpoint = await getHttpEndpoint({ network: "testnet" });
+    const client = new TonClient({ endpoint });
 
-    await counter.sendDeploy(provider.sender(), toNano('0.05'));
+    // prepare Counter's initial code and data cells for deployment
+    const counterCode = Cell.fromBoc(fs.readFileSync("build/counter.cell"))[0];
+    const initialCounterValue = Date.now();
+    const counter = Counter.createForDeploy(counterCode, initialCounterValue);
 
-    await provider.waitForDeploy(counter.address);
+    // exit if contract is already deployed
+    console.log("Contract address: ", counter.address.toString());
 
-    // run methods on `counter`
+    if (await client.isContractDeployed(counter.address)) {
+        return console.log("Counter already deployed");
+    }
+
+    // open wallet v4 (notice the correct wallet version here)
+    const mnemonic = "unfold sugar watter ...";
+    const key = await mnemonicToWalletKey(mnemonic.split(" "));
+    const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+
+    if (!await client.isContractDeployed(wallet.address)) {
+        return console.log("wallet is not deployed")
+    }
+
+    // open wallet and read the current seqno of the wallet
+    const walletContract = client.open(wallet);
+    const walletSender = walletContract.sender(key.secretKey);
+    const seqno = await walletContract.getSeqno();
+
+    // send the deploy transaction
+    const counterContract = client.open(counter);
+    await counterContract.sendDeploy(walletSender);
+
+    // wait until confirmed
+    let currentSeqno = seqno;
+
+    while (currentSeqno == seqno) {
+        console.log("waiting for deploy transaction to confirm...");
+        await sleep(1500);
+        currentSeqno = await walletContract.getSeqno();
+    }
+
+    console.log("deploy transaction confirmed!");
+}
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
